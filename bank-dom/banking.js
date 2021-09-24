e1d3cc1d7d03a245ec55ed8d46bd6c5bf2f00b0a
kaderabdul22 <mostafakamal9300@gmail.com>
2021-09-24 11:18:03 +0600

    due to net issue i'll solve in next

---
+++ bank-dom/banking.js
@@ -0,0 +1,34 @@
+// console.log('f')
+
+
+// const depostInput = document.getElementById('deposit-input').value;
+// console.log(depostInput)
+const depostInput = document.getElementById('deposit-input').value;
+console.log(depostInput)
+
+
+// document.getElementById('deposit-button')
+// .addEventListener('click',function(){
+//     console.log()
+// })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
