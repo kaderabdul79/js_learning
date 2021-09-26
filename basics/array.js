081558fef73b78ffe76aaea8c9a62264f694a129
kaderabdul22 <mostafakamal9300@gmail.com>
2021-09-26 23:04:22 +0600

    different array method

--- basics/array.js
+++ basics/array.js
@@ -5,3 +5,12 @@ const ages = [21,22,23,45,55]
 // }
 
 ages.forEach(age => {console.log(age)})
+//insert last
+ages.push(1)
+//insert 1st
+ages.unshift(33)
+//remove last
+ages.pop(1)
+//remove first
+ages.shift(ages)
+ console.log(ages)
